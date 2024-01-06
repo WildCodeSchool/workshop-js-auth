536ee44db2c3172945033f55c1761b00d61df237
romaing <rocambille@gmail.com>
2024-01-06 13:25:03 +0100

    fixed comments in authControllers.js

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -3,13 +3,13 @@ const tables = require("../tables");
 
 const login = async (req, res, next) => {
   try {
-    // Fetch all users from the database
+    // Fetch a specific user from the database based on the provided email
     const user = await tables.user.readByEmail(req.body.email);
 
     if (user == null || user.password !== req.body.password) {
       res.sendStatus(422);
     } else {
-      // Respond with the users in JSON format
+      // Respond with the user in JSON format (but without the hashed password)
       res.json(user);
     }
   } catch (err) {
