31a0ecb18a6000ccf25c2022de8aa3324412cfd2
Romain Guillemot <rocambille@gmail.com>
2024-03-05 12:00:34 +0100

    Update authControllers.js

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -24,7 +24,7 @@ const login = async (req, res, next) => {
       delete user.hashed_password;
 
       const token = await jwt.sign(
-        { sub: user.id, isAdmin: user.isAdmin },
+        { sub: user.id, isAdmin: user.is_admin },
         process.env.APP_SECRET,
         {
           expiresIn: "1h",
